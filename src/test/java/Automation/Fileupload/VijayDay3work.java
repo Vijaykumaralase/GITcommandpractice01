151ecd133c3c2d4087984a344b3dfb7d98b01941
vijaykumaralase@gmail.com <vijaykumaralase@gmail.com>
2023-03-09 08:16:13 +0530

    vijay chnages parallel to Snehas work

---
+++ src/test/java/Automation/Fileupload/VijayDay3work.java
@@ -0,0 +1,14 @@
+package Automation.Fileupload;
+
+public class VijayDay3work {
+	public void Day2work()
+	{
+	System.out.println("This is 3rd day work done by Vijay");
+	}
+	
+	public void SnehaDay03()
+	{
+		System.out.println("This work is done by Vijay Next day 09-03-2023 8:15 am");
+	}
+
+}
--- src/test/java/Automation/Fileupload/vijaySeconddaywork.java
+++ src/test/java/Automation/Fileupload/vijaySeconddaywork.java
@@ -7,4 +7,13 @@ public class vijaySeconddaywork {
 		System.out.println("This is the change 01 of day 2");
 	}
 
+	public void Day2work()
+	{
+	System.out.println("This is second day work done by Vijay");
+	}
+	
+	public void SnehaDay03()
+	{
+		System.out.println("This work is done by Vijay Next day 09-03-2023 8:13 am");
+	}
 }
